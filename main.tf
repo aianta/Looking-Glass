06143c9ab040d6dc791e9266719065a7f070bc1c
Alex Ianta <aianta@dal.ca>
2021-01-06 13:00:05 -0400

    try with different rewrite annotation

--- main.tf
+++ main.tf
@@ -738,7 +738,7 @@ resource "kubernetes_ingress" "looking_glass_ingress"{
       name = "looking-glass-ingress"
       annotations = {
         "kubernetes.io/ingress.class" = "nginx"
-        "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+        "nginx.org/rewrites" = "serviceName=${kubernetes_service.kibana_service.metadata.0.name} rewrite=/;serviceName=${kubernetes_service.kafka_service.metadata.0.name} rewrite=/"
       }
     }
 
