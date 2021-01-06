813c79ab0e3504bd33a74ff8c1e9415c2d92b992
Alex Ianta <aianta@dal.ca>
2021-01-06 13:33:56 -0400

    testing basic auth

--- main.tf
+++ main.tf
@@ -320,6 +320,7 @@ resource "kubernetes_deployment" "kibana"{
                     }
 
                     env{
+                        # Need to set this to match ingress configuration below
                         name="SERVER_BASEPATH"
                         value = "/nims/kibana"
                     }
@@ -742,8 +743,9 @@ resource "kubernetes_ingress" "looking_glass_ingress"{
     metadata {
       name = "looking-glass-ingress"
       annotations = {
-        "kubernetes.io/ingress.class" = "nginx"
         "nginx.org/rewrites" = "serviceName=${kubernetes_service.kibana_service.metadata.0.name} rewrite=/;serviceName=${kubernetes_service.kafka_service.metadata.0.name} rewrite=/"
+        "nginx.ingress.kubernetes.io/auth-type" = "basic"
+        "nginx.ingress.kubernetes.io/auth-secret" = "basic-auth"
       }
     }
 
